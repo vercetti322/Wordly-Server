104b9d8383b9932e688870957146b4551ecb4520
vercetti322 <vercettitommy322@gmail.com>
2024-12-19 11:12:17 +0530

    project init!

---
+++ src/main/java/io/game/Wordly/WordlyApplication.java
@@ -0,0 +1,11 @@
+package io.game.Wordly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordlyApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(WordlyApplication.class, args);
+	}
+}
---
+++ src/test/java/io/game/Wordly/WordlyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.game.Wordly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
